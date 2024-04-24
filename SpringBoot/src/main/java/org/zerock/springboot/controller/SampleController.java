e8a572fffd26cb2a2e8ce2e4a4279f848363901d
seohyun1001 <sh54991210@gmail.com>
2024-04-24 10:20:45 +0900

    10:20 / [spring boot] Thymeleaf 링크 처리, 인라인 처리

--- SpringBoot/src/main/java/org/zerock/springboot/controller/SampleController.java
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/SampleController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -23,5 +27,43 @@ public class SampleController {
         List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
         model.addAttribute("list", list);
     }
+
+    // Thymeleaf 인라인 처리
+    class SampleDTO {
+        private String p1, p2, p3;
+        public String getP1() {
+            return p1;
+        }
+
+        public String getP2() {
+            return p2;
+        }
+
+        public String getP3() {
+            return p3;
+        }
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        List<String> strList = IntStream.range(1,10)
+                .mapToObj(i -> "Data" + i)
+                .collect(Collectors.toList());
+
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+        model.addAttribute("map", map);
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+
+        model.addAttribute("dto", sampleDTO);
+    }
+
 
 }
