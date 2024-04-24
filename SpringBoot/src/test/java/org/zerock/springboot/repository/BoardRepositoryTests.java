06617c329f3fc03e6b77711363044b45a4e699ee
seohyun1001 <sh54991210@gmail.com>
2024-04-24 16:16:38 +0900

    16:15 / [spring boot] JpaRepository paging / setting querydsl

--- SpringBoot/src/test/java/org/zerock/springboot/repository/BoardRepositoryTests.java
+++ SpringBoot/src/test/java/org/zerock/springboot/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.springboot.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -49,6 +54,21 @@ public class BoardRepositoryTests {
     public void testDelete() {
         Long bno = 1L;
         boardRepository.deleteById(bno);
+    }
+
+
+
+    @Test
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(5, 10, Sort.by("bno").descending());
+        Page<Board> result = boardRepository.findAll(pageable);
+        log.info("total count : " + result.getTotalElements());
+        log.info("total pages : " + result.getTotalPages());
+        log.info("page number : " + result.getNumber());
+        log.info("page size : " + result.getSize());
+        List<Board> todoList = result.getContent();
+        todoList.forEach(board -> log.info(board));
+
     }
 }
 
