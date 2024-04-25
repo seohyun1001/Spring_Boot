4754aef84c114c7f7d2fce88e1b5e26095b495d9
seohyun1001 <sh54991210@gmail.com>
2024-04-25 15:13:56 +0900

    14:20 / [spring boot] /board/list 등록 기능 추가

--- SpringBoot/src/main/java/org/zerock/springboot/controller/BoardController.java
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/BoardController.java
@@ -1,11 +1,15 @@
 package org.zerock.springboot.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springboot.dto.BoardDTO;
 import org.zerock.springboot.dto.PageRequestDTO;
 import org.zerock.springboot.dto.PageResponseDTO;
@@ -24,7 +28,28 @@ public class BoardController {
         PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
         log.info(responseDTO);
         model.addAttribute("responseDTO", responseDTO);
+    }
 
+    @GetMapping("register")
+    public void registerGET() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("board post register------------------");
+
+        if (bindingResult.hasErrors()) {
+            log.info("has error-----------------");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+        return "redirect:/board/list";
     }
 
 }
