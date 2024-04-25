bc62a74a31afcdb5b93fe149aa3a832d8a23e1cc
seohyun1001 <sh54991210@gmail.com>
2024-04-25 12:23:35 +0900

    12:20 / [spring boot] setting board layout and screen

---
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/BoardController.java
@@ -0,0 +1,25 @@
+package org.zerock.springboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.springboot.dto.PageRequestDTO;
+import org.zerock.springboot.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
+@Log4j2
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+    }
+
+}
