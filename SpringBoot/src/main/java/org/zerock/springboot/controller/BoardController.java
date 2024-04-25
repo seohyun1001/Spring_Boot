486d1f252335069d1961bbbde71a40849f034e93
seohyun1001 <sh54991210@gmail.com>
2024-04-25 12:51:05 +0900

    12:50 / [spring boot] setting board screen

--- SpringBoot/src/main/java/org/zerock/springboot/controller/BoardController.java
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.springboot.dto.BoardDTO;
 import org.zerock.springboot.dto.PageRequestDTO;
+import org.zerock.springboot.dto.PageResponseDTO;
 import org.zerock.springboot.service.BoardService;
 
 @Controller
@@ -19,6 +21,9 @@ public class BoardController {
 
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        log.info(responseDTO);
+        model.addAttribute("responseDTO", responseDTO);
 
     }
 
