1f37b12d90b80f7e26fed3be0f1f624ddc05d9ad
seohyun1001 <sh54991210@gmail.com>
2024-04-29 12:39:39 +0900

    12:35 / add validity check and error filter

--- SpringBoot/src/main/java/org/zerock/springboot/controller/ReplyController.java
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/ReplyController.java
@@ -1,15 +1,19 @@
 package org.zerock.springboot.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.springboot.dto.ReplyDTO;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -21,9 +25,17 @@ public class ReplyController {
     // @ApiOperation(value, notes) 대신에 Tag 씀
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     // ResponseEntity : 데이터를 http 상태 코드와 같이 앞단에 전달하기
-    public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public ResponseEntity<Map<String, Long>> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException {
         log.info(replyDTO);
-        Map<String, Long> resultMap = Map.of("rno", 2L);
+
+        // 오류 및 유효성 체크 등, 오류 발생시 에러 표시
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", 123L);
+
         return ResponseEntity.ok(resultMap);
     }
 
--- SpringBoot/src/main/java/org/zerock/springboot/dto/ReplyDTO.java
+++ SpringBoot/src/main/java/org/zerock/springboot/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package org.zerock.springboot.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +17,15 @@ public class ReplyDTO {
 
     private Long rno;
 
+    @NotNull
     private Long bno;
 
+    @NotEmpty
     private String replyText;
+
+    @NotEmpty
     private String replyer;
+
     private LocalDateTime regDate, modDate;
 
 }
