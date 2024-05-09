194e242995e187f0872b196a30dddd4ff1cc9ca8
seohyun1001 <sh54991210@gmail.com>
2024-05-09 10:41:49 +0900

    10:40 / @EntityGraph와 조회 테스트

--- SpringBoot/src/main/java/org/zerock/springboot/repository/BoardRepository.java
+++ SpringBoot/src/main/java/org/zerock/springboot/repository/BoardRepository.java
@@ -1,11 +1,19 @@
 package org.zerock.springboot.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.springboot.domain.Board;
 import org.zerock.springboot.repository.search.BoardSearch;
 
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
-    @Query(value = "select now()", nativeQuery = true)
-    String getTime();
+//    @Query(value = "select now()", nativeQuery = true)
+//    String getTime();
+
+    @EntityGraph(attributePaths = {"imageSet"})
+    @Query("select b from Board b where b.bno =:bno")
+    Optional<Board> findByIdWithImages(Long bno);
+
 }
--- SpringBoot/src/test/java/org/zerock/springboot/repository/BoardRepositoryTests.java
+++ SpringBoot/src/test/java/org/zerock/springboot/repository/BoardRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.zerock.springboot.domain.Board;
+import org.zerock.springboot.domain.BoardImage;
 import org.zerock.springboot.dto.BoardListReplyCountDTO;
 
 import java.util.List;
@@ -134,14 +135,19 @@ public class BoardRepositoryTests {
     @Test
     public void testReadWithImages() {
         // PK를 이용해 게시글 select 실행
-        Optional<Board> result = boardRepository.findById(1L);
+        Optional<Board> result = boardRepository.findByIdWithImages(1L);
         // 데이터가 없는 경우 에러발생
         Board board = result.orElseThrow();
 
         log.info(board);
         log.info("---------------------------------");
-        log.info(board.getImageSet());
+//        log.info(board.getImageSet());
         // -> 데이터베이스와 연결이 끝난 상태이므로 'no session'이라는 에러 메세지가 뜬다
         // -> 해결방법 : @Transactional을 추가함 -> 커밋이 되기 전까지 실행한 쿼리를 하나로 인식함
+
+        for (BoardImage boardImage : board.getImageSet()){ // list를 log로 출력
+            log.info(boardImage);
+        }
     }
+
 }
