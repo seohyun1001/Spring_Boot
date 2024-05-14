c803abccd19a2b7ab58f60c53ab31f9e11bec98f
seohyun1001 <sh54991210@gmail.com>
2024-05-14 11:22:32 +0900

    11:20 / 어노테이션을 이용한 권한 체크

--- SpringBoot/src/main/java/org/zerock/springboot/config/CustomSecurityConfig.java
+++ SpringBoot/src/main/java/org/zerock/springboot/config/CustomSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class CustomSecurityConfig {
 
     @Bean
