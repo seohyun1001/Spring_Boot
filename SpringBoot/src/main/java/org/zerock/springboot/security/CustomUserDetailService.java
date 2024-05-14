c50798e41a0afff3a2b1b8c57c2396016795abeb
seohyun1001 <sh54991210@gmail.com>
2024-05-14 11:02:48 +0900

    11:00 / UserDetails라는 반환 타입

--- SpringBoot/src/main/java/org/zerock/springboot/security/CustomUserDetailService.java
+++ SpringBoot/src/main/java/org/zerock/springboot/security/CustomUserDetailService.java
@@ -2,6 +2,7 @@ package org.zerock.springboot.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,6 +16,15 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("Load User By Username : " + username);
-        return null;
+
+        // 반환할 UserDetails 생성하기
+        UserDetails userDetails = User.builder()
+                .username("user1")
+                .password("1111")
+                .authorities("ROLE_USER")
+                .build();
+
+        return userDetails;
+        // -> 이 코드만 추가한 후 실행하면 password encoder가 없어서 오류가 남
     }
 }
