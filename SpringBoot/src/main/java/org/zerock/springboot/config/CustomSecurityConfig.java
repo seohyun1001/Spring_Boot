0253368a102d359efe8cf1750a335393f8bec5fe
seohyun1001 <sh54991210@gmail.com>
2024-05-14 10:21:02 +0900

    10:20 / 정적 자원 처리

--- SpringBoot/src/main/java/org/zerock/springboot/config/CustomSecurityConfig.java
+++ SpringBoot/src/main/java/org/zerock/springboot/config/CustomSecurityConfig.java
@@ -1,19 +1,28 @@
 package org.zerock.springboot.config;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 public class CustomSecurityConfig {
 
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        log.info("-------------------- configure --------------------");
+//        return http.build();
+//    }
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        log.info("-------------------- configure --------------------");
-        return http.build();
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("-------------------- web configure --------------------");
+        //static 폴더 안에 존재하는 정적 리소스는 로그인 과정을 거치지 않고 실행 가능
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
