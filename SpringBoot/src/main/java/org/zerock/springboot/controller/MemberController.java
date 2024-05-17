56c39126e92c972cc7575855f9a790ede7506bba
seohyun1001 <sh54991210@gmail.com>
2024-05-17 11:27:12 +0900

    11:30 / 회원가입 서비스 계층 처리(MemberController/join 수정) + 설명 추가

--- SpringBoot/src/main/java/org/zerock/springboot/controller/MemberController.java
+++ SpringBoot/src/main/java/org/zerock/springboot/controller/MemberController.java
@@ -6,13 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.springboot.dto.MemberJoinDTO;
+import org.zerock.springboot.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 @Log4j2
 @RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String error, String logout) {
@@ -29,10 +33,22 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joingPOST(MemberJoinDTO memberJoinDTO) {
+    public String joingPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes) {
         log.info("join post --------------------");
         log.info(memberJoinDTO);
-        return "redirect:/board/list";
+
+        try {
+            // 회원가입 서비스 실행
+            memberService.join(memberJoinDTO);
+            // 기존에 아이디가 존재할 경우 에러 발생
+        } catch (MemberService.MIdExistException e) {
+            // 에러 발생시 redirect 페이지에 error:mid 값을 가지고 ->
+            redirectAttributes.addFlashAttribute("error", "mid");
+            // -> member join 페이지로 이동
+            return "redirect:/member/join";
+        }
+        redirectAttributes.addFlashAttribute("result", "success");
+        return "redirect:/member/login";
     }
 
 }
--- SpringBoot/src/main/java/org/zerock/springboot/service/MemberServiceImpl.java
+++ SpringBoot/src/main/java/org/zerock/springboot/service/MemberServiceImpl.java
@@ -23,21 +23,27 @@ public class MemberServiceImpl implements MemberService {
     @Override
     public void join(MemberJoinDTO memberJoinDTO) throws MIdExistException {
 
+        // 화면헤서 가지고 온 ID를 저장
         String mid = memberJoinDTO.getMid();
+        // JPA에서 지원하는 ID 존재 여부 확인 메서드 실행
         boolean exist = memberRepository.existsById(mid);
-
+        // ID가 이미 존재하면 에러를 발생시킴
         if (exist) {
             throw new MIdExistException();
         }
 
+        // ID가 존재하지 않으면 Member 객체로 변환
         Member member = modelMapper.map(memberJoinDTO, Member.class);
+        // 비밀번호 암호화
         member.changePassword(passwordEncoder.encode(memberJoinDTO.getMpw()));
+        // 권한 설정
         member.addRole(MemberRole.USER);
 
         log.info("==========================================");
         log.info(member);
         log.info(member.getRoleSet());
 
+        // 데이터베이스에 저장
         memberRepository.save(member);
     }
 }
